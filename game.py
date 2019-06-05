54ce7dd0fbc68a41bb3ec68adaaf775d706338cc
Zach-Diamond <50925443+Zach-Diamond@users.noreply.github.com>
2019-06-05 18:56:13 -0400

    Capturing Inputs

--- game.py
+++ game.py
@@ -1,1 +1,16 @@
 print("Rock, Paper, Scissors, Shoot!")
+
+# Capture Input
+
+user_choice = input('Please Select ROCK, PAPER, or SCISSORS: ')
+print('-------------')
+print("YOU CHOSE: ",user_choice)
+
+
+# Validating  Inputs
+
+# Generate Computer Selection
+
+# Determine Winner
+
+# Display Final Outcomes
