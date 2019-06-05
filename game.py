a085a67672df91ceb13586a44faf29ff35bdc959
Zach-Diamond <50925443+Zach-Diamond@users.noreply.github.com>
2019-06-05 19:26:18 -0400

    Generating Random Selection

--- game.py
+++ game.py
@@ -1,3 +1,6 @@
+# Modules
+import random
+
 print("Rock, Paper, Scissors, Shoot!")
 
 # Capture Input
@@ -6,21 +9,41 @@ user_choice = input('Please Select ROCK, PAPER, or SCISSORS: ')
 print('-------------')
 print("YOU CHOSE: ",user_choice)
 
-
 # Validating  Inputs
 
 options = ['rock','paper','scissors']
 
-if user_choice.lower() in options:
-    user_choice
-else:
+if user_choice.lower() not in options:
     print("That's an invalid choice. Try again.")
     exit()
 
 # Generate Computer Selection
 
-print("Generating...")
+computer_choice = random.choice(options).capitalize()
+print("COMPUTER CHOSE :",computer_choice)
 
 # Determine Winner
 
+
+
+
+
+
+
+
+
+
 # Display Final Outcomes
+
+
+
+
+
+
+
+### NO LONGER USED
+# if user_choice.lower() in options:
+#     pass
+# else:
+#     print("That's an invalid choice. Try again.")
+#     exit()
